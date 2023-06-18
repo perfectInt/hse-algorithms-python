1709b6aab71c0d1c83b3583ed9659d2e0b752a94
yergoff <ruslanja1@mail.ru>
2023-06-18 14:21:01 +0300

    Added Ford-Bellman algorithm

---
+++ ford_bellman.py
@@ -0,0 +1,18 @@
+# This task from https://acmp.ru/index.asp?main=task&id_task=138
+n, m = map(int, input().split())
+edges = []
+d = [30000] * n
+for i in range(m):
+    u, v, w = map(int, input().split())
+    edges.append([u - 1, v - 1, w])
+
+d[0] = 0
+for k in range(1, n):
+    for edge in edges:
+        start = edge[0]
+        finish = edge[1]
+        weight = edge[2]
+        if d[start] + weight < d[finish] and d[start] < 30000:
+            d[finish] = d[start] + weight
+
+print(*d)
