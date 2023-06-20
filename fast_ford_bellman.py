028578a2c09cfc7327fd75f667337f27f8437da9
yergoff <ruslanja1@mail.ru>
2023-06-20 08:35:30 +0300

    Added Ford-Bellman better implementation

--- fast_ford_bellman.py
+++ fast_ford_bellman.py
@@ -7,12 +7,15 @@ for i in range(m):
     edges.append([u - 1, v - 1, w])
 
 d[0] = 0
-for k in range(1, n):
+ok = False
+while not ok:
+    ok = True
     for edge in edges:
         start = edge[0]
         finish = edge[1]
         weight = edge[2]
         if d[start] + weight < d[finish] and d[start] < 30000:
             d[finish] = d[start] + weight
+            ok = False
 
 print(*d)
