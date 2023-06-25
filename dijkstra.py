46ab5409782665091768f68bac8c84747517046e
yergoff <ruslanja1@mail.ru>
2023-06-25 11:55:57 +0300

    Fixed some code

--- dijkstra.py
+++ dijkstra.py
@@ -1,39 +1,37 @@
 import heapq
 
 n, m, s = map(int, input().split())
-edges = []
+graph = [[] for i in range(n)]
 INF = 2 ** 20
 dist = [INF] * n
-visited = [False] * n
 for i in range(m):
     u, v, w = map(int, input().split())
-    edges.append((u - 1, v - 1, w))
-dist[s - 1] = 0
+    graph[u - 1].append([v - 1, w])
 
 
-def dijkstra(edges, start_vertex):
-    global visited, dist
-    edge_list = []
-    graph = [[] for _ in range(n)]
-    for edge in edges:
-        graph[edge[0]].append([edge[1], edge[2]])
-    visited[start_vertex] = True
-    dist[start_vertex] = 0
-    for dest, weight in graph[start_vertex]:
-        heapq.heappush(edge_list, (weight, start_vertex, dest))
-    while len(edge_list) > 0:
-        weight, src, dest = heapq.heappop(edge_list)
-        if visited[dest]:
+def dijkstra(start):
+    global dist, graph
+    dist[start] = 0
+
+    queue = [(0, start)]
+
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if current_distance > dist[current_node]:
             continue
-        dist[dest] = dist[src] + weight
-        visited[dest] = True
-        for new_dest, new_weight in graph[dest]:
-            if not visited[new_dest]:
-                heapq.heappush(edge_list, (new_weight, dest, new_dest))
+
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+            if distance < dist[neighbor]:
+                dist[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+
     return dist
 
 
-print(*dijkstra(edges, s - 1))
+print(*dijkstra(s - 1))
 
 
 
